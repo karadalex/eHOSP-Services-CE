1b25eacd2be4546fbf5e501b4c5afb604551c433
karadalex <karadalex@gmail.com>
2016-03-07 22:28:17 +0200

    Edited styling of homepage!
    Edited styling of homepagewith css and javascript

---
+++ src/js/dynamic.js
@@ -0,0 +1,18 @@
+function getWidth() {
+	var width = window.innerWidth;
+	return width;
+}
+
+function getHeight() {
+	var height = window.innerHeight;
+	return height;
+}
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+var padding_space = map_range(getWidth(), 0, 1366, 3, 80);
+for (element in document.getElementsByClassName('menu')) {
+	element.style.padding = "5px 80px";
+}
