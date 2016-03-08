965b8ea866cf04c068e573dc7c6423eb83fdee52
karadalex <karadalex@gmail.com>
2016-03-08 02:22:45 +0200

    Added tablet and smartphone version.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	$("#toggleMenu").click(function() {
+		$(".row ul").toggle('slide', {direction: 'left'}, 500);
+	});
 
+	$("#menuOut").click(function() {
+		$(".row ul").toggle('slide', {direction: 'left'}, 500);
+	});
 
 });
