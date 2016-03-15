5cd7fa9716fce05faf7c36b4fe308ad6bff13c1c
Alex Karadimos <karadalex@gmail.com>
2016-03-15 23:37:31 +0200

    Created js-minify task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ var gulp = require('gulp')
     uglify = require('gulp-uglify');
 
 // Minify javascript files and put the minified version in the dist folder
-gulp.task('default', function(){
+gulp.task('js-minify', function(){
     gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+    gulp.src('src/js/foundation/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/foundation'));
+    gulp.src('src/js/vendor/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/vendor'));
 });
+
+// Deafult task to run all of the above tasks
+gulp.task('default', ['js-minify']);
