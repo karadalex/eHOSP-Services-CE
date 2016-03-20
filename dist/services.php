5149f57e99d6ab9a978a0bb762c54dd8b785159b
Alex Karadimos <karadalex@gmail.com>
2016-03-20 21:21:15 +0200

    Adjusted dist directory (project's build) to D.R.Y. code removal

--- dist/services.php
+++ dist/services.php
@@ -2,21 +2,10 @@
 <html class="no-js" lang="en">
 <head>
 	<title>eHOSP - Hospital on the cloud</title>
-
-	<meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+	<?php
+		include 'includes/html/head.html';
+	?>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-
-	<link rel="stylesheet" href="css/foundation.css" />
-    <link rel="stylesheet" href="css/custom.css" />
-    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300' rel='stylesheet' type='text/css'>
-
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/script.js"></script>
-    <script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.core.js"></script>
-    <script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.slide.js"></script>
 </head>
 <body>
 	<div id="container">
--- dist/services/research-platform/contact.php
+++ dist/services/research-platform/contact.php
@@ -2,24 +2,15 @@
 <html class="no-js" lang="en">
 <head>
 	<title>eHOSP - Contact</title>
-
-	<meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-	<link rel="stylesheet" href="css/foundation.css" />
-    <link rel="stylesheet" href="css/custom.css" />
-    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300' rel='stylesheet' type='text/css'>
-
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/script.js"></script>
-    <script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.core.js"></script>
-    <script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.slide.js"></script>
+	<?php
+		include '../includes/html/head.html';
+	?>
 </head>
 <body>
 	<div id="container">
-		<div w3-include-HTML="includes/html/header.html"></div>
-		<script src="js/w3-include-HTML.js"></script>
+		<?php
+			include '../includes/html/header.html';
+		?>
 
 		<div class="row">
 	        <h1>Contact us</h1>
@@ -27,14 +18,12 @@
 				Welcome to eHOSP, the hospital on the cloud,
 				a platform for e-health and e-medicine!
 			</p>
-      	</div> 
+      	</div>
 	</div>
 
-	<script src="js/foundation.min.js"></script>
+	<script src="../../js/foundation.min.js"></script>
     <script>
     	$(document).foundation();
     </script>
-
-    <script type="text/javascript" src="js/dynamic.js"></script>
 </body>
 </html>
