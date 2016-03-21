edfb70e118274aa77736566015932a35b11a0a72
Alex Karadimos <karadalex@gmail.com>
2016-03-21 19:41:24 +0200

    add: Added init.php. This file links core's modules so that they can be used in all pages, by just referencing init.php

--- src/core/init.php
+++ src/core/init.php
@@ -1,4 +1,10 @@
 <?php
-  session_start();
-  require 'db/connect.php';
+    session_start();
+    error_reporting(0);
+
+    include 'db/connect.php';
+    include 'methods/general.php';
+    include 'methods/users.php';
+
+    $errors = array();
 ?>
