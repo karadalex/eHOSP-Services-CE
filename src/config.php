1aceda2564b6362e7ad940756059cd7140f700fb
Alex Karadimos <karadalex@gmail.com>
2016-04-01 02:09:08 +0300

    feat: Added web application's PHP configuration file

---
+++ src/config.php
@@ -0,0 +1,24 @@
+<?php
+    /***************************************************************************
+    *
+    * CONFIGURATION FILE:
+    * Use this file to installand configure the application
+    * @return : array with application's configuration information
+    *
+    * How to use it in project:
+    * $config = include('config.php');
+    *
+    ***************************************************************************/
+
+    return array(
+        'APP_NAME' => 'eHosp',
+        'APP_HOSTNAME' => 'localhost',
+        'APP_USERNAME' => 'root',
+
+        'DB_NAME' => 'eHosp',
+        'DB_HOSTNAME' => 'localhost',
+        'DB_PORT' => 3306,
+        'DB_USERNAME' => 'root',
+        'DB_ROOT_PASSWORD' => 'rootpsswd'
+    );
+?>
