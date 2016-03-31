b40475434ff3be4c297fae8b33ab3f1d48fe3cdc
Alex Karadimos <karadalex@gmail.com>
2016-04-01 02:39:22 +0300

    Dded more tables and relations

--- src/core/db/ehosp.sql
+++ src/core/db/ehosp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Mar 30, 2016 at 04:09 PM
+-- Generation Time: Mar 31, 2016 at 04:37 PM
 -- Server version: 5.5.47
 -- PHP Version: 5.5.31
 
@@ -36,6 +36,41 @@ CREATE TABLE `3d_bioprinting` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `arduino_log`
+--
+
+CREATE TABLE `arduino_log` (
+  `ard_log_id` int(11) NOT NULL,
+  `arduino_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `arduino_registry`
+--
+
+CREATE TABLE `arduino_registry` (
+  `arduino_id` int(11) NOT NULL,
+  `nxt_link_connection` int(11) NOT NULL,
+  `raspberrypi_link_connection` int(11) NOT NULL,
+  `model` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bot_doctor_log`
+--
+
+CREATE TABLE `bot_doctor_log` (
+  `log_id` int(11) NOT NULL,
+  `bot_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `bot_doctor_registry`
 --
 
@@ -51,7 +86,67 @@ CREATE TABLE `bot_doctor_registry` (
 
 CREATE TABLE `doctors_registry` (
   `doctor_id` int(11) NOT NULL,
-  `specialty` enum('','','','') NOT NULL
+  `specialty` enum('ALLERGY & IMMUNOLOGY','ADOLESCENT MEDICINE','ANAESTHESIOLOGY','PATHOLOGY','CARDIOLOGY','PEDIATRIC CARDIOLOGY','CARDIOTHORACIC SURGERY','CHILD/ADOLESCENT PSYCHIATRY & PSYCHOTHERAPY','CLINICAL NEUROPHYSIOLOGY','DERMATO-VENEREOLOGY','EMERGENCY MEDICINE','ENDOCRINOLOGY','GASTROENTEROLOGY','GENERAL PRACTICE','GERIATRICS','OBSTETRICS & GYNAECOLOGY','HEALTH INFORMATICS','HOSPICE & PALLIATIVE MEDICINE','INFECTIOUS DISEASE','INTERNAL MEDICINE','INTERVENTIONAL RADIOLOGY','VASCULAR MEDICINE','MICROBIOLOGY','NEPHROLOGY','NEUROLOGY','NEUROSURGERY','NUCLEAR MEDICINE','OCCUPATIONAL MEDICINE','OPTHALMOLOGY','ORTHOPAEDICS','ORAL & MAXILLOFACIAL SURGERY','OTORHINOLARYNGOLOGY','PAEDIATRICS','PAEDIATRIC ALLERGOLOGY','PAEDIATRIC ENDOCRINOLLOGY & DIABETES','PAEDIATRIC GASTROENTEROLOGY, HEPATOLOGY & NUTRITION','PAEDIATRIC HAEMATOLOGY & ONCOLOGY','PAEDIATRIC INFECTIOUS DISEASES','NEONATOLOGY','PAEDIATRIC NEPHROLOGY','PAEDIATRIC RESPIRATORY MEDICINE','PAEDIATRIC RHEUMATOLOGY','PAEDIATRIC SURGERY','PHYSICAL MEDICINE & REHABILITATION','PLASTIC, RECONSTRUCTIVE & AESTHETIC SURGERY','PODIATRIC MEDICINE','PULMONOLOGY','PSYCHIATRY','PUBLIC HEALTH','RADIOLOGY','SPORTS MEDICINE','NEURORADIOLOGY','RADIOTHERAPY','GENERAL SURGERY','UROLOGY','VASCULAR SURGERY') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_log`
+--
+
+CREATE TABLE `doctor_log` (
+  `log_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nxt_log`
+--
+
+CREATE TABLE `nxt_log` (
+  `nxt_log_id` int(11) NOT NULL,
+  `nxt_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nxt_registry`
+--
+
+CREATE TABLE `nxt_registry` (
+  `nxt_id` int(11) NOT NULL,
+  `arduino_link_connection` int(11) NOT NULL,
+  `raspberrypi_link_connection` int(11) NOT NULL,
+  `model` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `raspberrypi_log`
+--
+
+CREATE TABLE `raspberrypi_log` (
+  `rasp_log_id` int(11) NOT NULL,
+  `raspberrypi_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `raspberrypi_registry`
+--
+
+CREATE TABLE `raspberrypi_registry` (
+  `raspberrypi_id` int(11) NOT NULL,
+  `server_link_connection` int(11) NOT NULL,
+  `arduino_link_connection` int(11) NOT NULL,
+  `nxt_link_connection` int(11) NOT NULL,
+  `model` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,9 +159,11 @@ CREATE TABLE `surgeries` (
   `surgery_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `doctor_id` int(11) NOT NULL,
+  `diagnosis_id` int(11) NOT NULL,
   `name` mediumtext NOT NULL,
   `category` enum('CENTRAL NERVOUS SYSTEM','PERIPHERAL NERVOUS SYSTEM','ENDOCRINE','EYE','EARS','RESPIRATORY','CARDIOVASCULAR','LYMPHATIC','GASTROINTESTINAL / MOUTH','URINARY','MALE REPRODUCTIVE','FEMALE REPRODUCTIVE','BONE, CARTILAGE & JOINT','MUSCLE / SOFT TISSUE','BREAST','SKIN','OTHER / UNGROUPED') NOT NULL,
   `type` enum('-CENTESIS','-CLASIA','-DESIS','-ECTOMY','-OPSY','-OSCOPY','-OSTOMY','-OTOMY','-PEXY','-PLASTY','-RRHAPHY') NOT NULL,
+  `3d_task_id` int(11) NOT NULL,
   `effectiveness` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL,
   `surgery_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -81,8 +178,11 @@ CREATE TABLE `user_diagnosis` (
   `diagnosis_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `doctor_id` int(11) NOT NULL,
+  `symptoms_id` int(11) NOT NULL,
+  `genetic_code_id` int(11) NOT NULL,
   `json_diagnosis_result` longtext NOT NULL COMMENT 'Diagnosis results stored as a JSON object',
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `medical_history_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -94,7 +194,19 @@ CREATE TABLE `user_diagnosis` (
 CREATE TABLE `user_genetic_code` (
   `genetic_code_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `medical_history_id` int(11) NOT NULL,
   `json_genetic_code` longtext NOT NULL COMMENT 'The geneticcode of the user stored as a JSON object'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_log`
+--
+
+CREATE TABLE `user_log` (
+  `log_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -179,6 +291,28 @@ ALTER TABLE `3d_bioprinting`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `arduino_log`
+--
+ALTER TABLE `arduino_log`
+  ADD PRIMARY KEY (`ard_log_id`,`arduino_id`),
+  ADD KEY `arduino_id` (`arduino_id`);
+
+--
+-- Indexes for table `arduino_registry`
+--
+ALTER TABLE `arduino_registry`
+  ADD PRIMARY KEY (`arduino_id`),
+  ADD KEY `nxt_link_connection` (`nxt_link_connection`),
+  ADD KEY `raspberrypi_link_connection` (`raspberrypi_link_connection`);
+
+--
+-- Indexes for table `bot_doctor_log`
+--
+ALTER TABLE `bot_doctor_log`
+  ADD PRIMARY KEY (`log_id`,`bot_id`),
+  ADD KEY `bot_id` (`bot_id`);
+
+--
 -- Indexes for table `bot_doctor_registry`
 --
 ALTER TABLE `bot_doctor_registry`
@@ -191,12 +325,51 @@ ALTER TABLE `doctors_registry`
   ADD PRIMARY KEY (`doctor_id`);
 
 --
+-- Indexes for table `doctor_log`
+--
+ALTER TABLE `doctor_log`
+  ADD PRIMARY KEY (`log_id`,`doctor_id`),
+  ADD KEY `doctor_id` (`doctor_id`);
+
+--
+-- Indexes for table `nxt_log`
+--
+ALTER TABLE `nxt_log`
+  ADD PRIMARY KEY (`nxt_log_id`,`nxt_id`),
+  ADD KEY `nxt_id` (`nxt_id`);
+
+--
+-- Indexes for table `nxt_registry`
+--
+ALTER TABLE `nxt_registry`
+  ADD PRIMARY KEY (`nxt_id`),
+  ADD KEY `raspberrypi_link_connection` (`raspberrypi_link_connection`),
+  ADD KEY `arduino_link_connection` (`arduino_link_connection`);
+
+--
+-- Indexes for table `raspberrypi_log`
+--
+ALTER TABLE `raspberrypi_log`
+  ADD PRIMARY KEY (`rasp_log_id`,`raspberrypi_id`),
+  ADD KEY `raspberrypi_id` (`raspberrypi_id`);
+
+--
+-- Indexes for table `raspberrypi_registry`
+--
+ALTER TABLE `raspberrypi_registry`
+  ADD PRIMARY KEY (`raspberrypi_id`),
+  ADD KEY `arduino_link_connection` (`arduino_link_connection`),
+  ADD KEY `nxt_link_connection` (`nxt_link_connection`);
+
+--
 -- Indexes for table `surgeries`
 --
 ALTER TABLE `surgeries`
   ADD PRIMARY KEY (`surgery_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `doctor_id` (`doctor_id`);
+  ADD KEY `doctor_id` (`doctor_id`),
+  ADD KEY `3d_task_id` (`3d_task_id`),
+  ADD KEY `diagnosis_id` (`diagnosis_id`);
 
 --
 -- Indexes for table `user_diagnosis`
@@ -204,13 +377,24 @@ ALTER TABLE `surgeries`
 ALTER TABLE `user_diagnosis`
   ADD PRIMARY KEY (`diagnosis_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `doctor_id` (`doctor_id`);
+  ADD KEY `doctor_id` (`doctor_id`),
+  ADD KEY `symptoms_id` (`symptoms_id`),
+  ADD KEY `genetic_code_id` (`genetic_code_id`),
+  ADD KEY `medical_history_id` (`medical_history_id`);
 
 --
 -- Indexes for table `user_genetic_code`
 --
 ALTER TABLE `user_genetic_code`
   ADD PRIMARY KEY (`genetic_code_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `medical_history_id` (`medical_history_id`);
+
+--
+-- Indexes for table `user_log`
+--
+ALTER TABLE `user_log`
+  ADD PRIMARY KEY (`log_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -250,6 +434,21 @@ ALTER TABLE `user_vital_signs`
 ALTER TABLE `3d_bioprinting`
   MODIFY `3d_task_id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `arduino_log`
+--
+ALTER TABLE `arduino_log`
+  MODIFY `ard_log_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `arduino_registry`
+--
+ALTER TABLE `arduino_registry`
+  MODIFY `arduino_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bot_doctor_log`
+--
+ALTER TABLE `bot_doctor_log`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `bot_doctor_registry`
 --
 ALTER TABLE `bot_doctor_registry`
@@ -259,6 +458,31 @@ ALTER TABLE `bot_doctor_registry`
 --
 ALTER TABLE `doctors_registry`
   MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `doctor_log`
+--
+ALTER TABLE `doctor_log`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `nxt_log`
+--
+ALTER TABLE `nxt_log`
+  MODIFY `nxt_log_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `nxt_registry`
+--
+ALTER TABLE `nxt_registry`
+  MODIFY `nxt_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `raspberrypi_log`
+--
+ALTER TABLE `raspberrypi_log`
+  MODIFY `rasp_log_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `raspberrypi_registry`
+--
+ALTER TABLE `raspberrypi_registry`
+  MODIFY `raspberrypi_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `surgeries`
 --
@@ -274,6 +498,11 @@ ALTER TABLE `user_diagnosis`
 --
 ALTER TABLE `user_genetic_code`
   MODIFY `genetic_code_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_log`
+--
+ALTER TABLE `user_log`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user_registry`
 --
@@ -297,27 +526,70 @@ ALTER TABLE `user_vital_signs`
 -- Constraints for table `3d_bioprinting`
 --
 ALTER TABLE `3d_bioprinting`
+  ADD CONSTRAINT `3d_bioprinting_ibfk_2` FOREIGN KEY (`genetic_code_id`) REFERENCES `user_genetic_code` (`genetic_code_id`),
   ADD CONSTRAINT `3d_bioprinting_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`);
+
+--
+-- Constraints for table `arduino_log`
+--
+ALTER TABLE `arduino_log`
+  ADD CONSTRAINT `arduino_log_ibfk_1` FOREIGN KEY (`arduino_id`) REFERENCES `arduino_registry` (`arduino_id`);
+
+--
+-- Constraints for table `bot_doctor_log`
+--
+ALTER TABLE `bot_doctor_log`
+  ADD CONSTRAINT `bot_doctor_log_ibfk_1` FOREIGN KEY (`bot_id`) REFERENCES `bot_doctor_registry` (`bot_id`);
+
+--
+-- Constraints for table `doctor_log`
+--
+ALTER TABLE `doctor_log`
+  ADD CONSTRAINT `doctor_log_ibfk_1` FOREIGN KEY (`doctor_id`) REFERENCES `doctors_registry` (`doctor_id`);
+
+--
+-- Constraints for table `nxt_log`
+--
+ALTER TABLE `nxt_log`
+  ADD CONSTRAINT `nxt_log_ibfk_1` FOREIGN KEY (`nxt_id`) REFERENCES `nxt_registry` (`nxt_id`);
+
+--
+-- Constraints for table `raspberrypi_log`
+--
+ALTER TABLE `raspberrypi_log`
+  ADD CONSTRAINT `raspberrypi_log_ibfk_1` FOREIGN KEY (`raspberrypi_id`) REFERENCES `raspberrypi_registry` (`raspberrypi_id`);
 
 --
 -- Constraints for table `surgeries`
 --
 ALTER TABLE `surgeries`
+  ADD CONSTRAINT `surgeries_ibfk_4` FOREIGN KEY (`diagnosis_id`) REFERENCES `user_diagnosis` (`diagnosis_id`),
+  ADD CONSTRAINT `surgeries_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`),
   ADD CONSTRAINT `surgeries_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctors_registry` (`doctor_id`),
-  ADD CONSTRAINT `surgeries_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`);
+  ADD CONSTRAINT `surgeries_ibfk_3` FOREIGN KEY (`3d_task_id`) REFERENCES `3d_bioprinting` (`3d_task_id`);
 
 --
 -- Constraints for table `user_diagnosis`
 --
 ALTER TABLE `user_diagnosis`
+  ADD CONSTRAINT `user_diagnosis_ibfk_5` FOREIGN KEY (`medical_history_id`) REFERENCES `user_medical_history` (`user_id`),
+  ADD CONSTRAINT `user_diagnosis_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`),
   ADD CONSTRAINT `user_diagnosis_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctors_registry` (`doctor_id`),
-  ADD CONSTRAINT `user_diagnosis_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`);
+  ADD CONSTRAINT `user_diagnosis_ibfk_3` FOREIGN KEY (`symptoms_id`) REFERENCES `user_symptoms` (`symptoms_id`),
+  ADD CONSTRAINT `user_diagnosis_ibfk_4` FOREIGN KEY (`genetic_code_id`) REFERENCES `user_genetic_code` (`genetic_code_id`);
 
 --
 -- Constraints for table `user_genetic_code`
 --
 ALTER TABLE `user_genetic_code`
+  ADD CONSTRAINT `user_genetic_code_ibfk_2` FOREIGN KEY (`medical_history_id`) REFERENCES `user_medical_history` (`user_id`),
   ADD CONSTRAINT `user_genetic_code_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`);
+
+--
+-- Constraints for table `user_log`
+--
+ALTER TABLE `user_log`
+  ADD CONSTRAINT `user_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_registry` (`user_id`);
 
 --
 -- Constraints for table `user_medical_history`
