081afcba2f6d426221e3a61e0d438378cc632928
karadalex <karadalex@gmail.com>
2016-04-02 15:06:13 +0300

    Added comments to clarify array elements

--- src/config.php
+++ src/config.php
@@ -11,16 +11,20 @@
     ***************************************************************************/
 
     return array(
+        // Application's Info
         'APP_NAME' => 'eHosp',
         'APP_HOSTNAME' => 'localhost',
-        'APP_USERNAME' => 'root',
+        'APP_ADMIN_USERNAME' => 'admin',
+        'APP_ADMIN_PASSWORD' => 'adminpsswd',
 
+        // Database's Info
         'DB_NAME' => 'ehosp',
         'DB_HOSTNAME' => 'localhost',
         'DB_PORT' => 3306,
-        'DB_USERNAME' => 'root',
+        'DB_ROOT_USERNAME' => 'root',
         'DB_ROOT_PASSWORD' => 'rootpsswd',
 
+        // Raspberry Pi's Info
         'RASPBERRYPI_HOSTNAME' => '192.168.x.x',
         'RASPBERRYPI_PORT' => 49153
     );
