2a2b8a449aa747ca927712f487e89c21f97ff73b
karadalex <karadalex@gmail.com>
2016-04-05 01:06:49 +0300

    Php code that loads the configuration info in the settings page

---
+++ src/core/sysconfig/load_config.php
@@ -0,0 +1,9 @@
+<?php
+	/********************************************************************************
+	*
+	* Load current configuration information anddisplay it in the settings page
+	* where it can be changed
+	*
+	********************************************************************************/
+
+?>
