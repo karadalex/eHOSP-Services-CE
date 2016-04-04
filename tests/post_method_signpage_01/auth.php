81057fe031ab2e3eb79c2a701fa1b3539d7947c2
karadalex <karadalex@gmail.com>
2016-04-04 23:00:13 +0300

    Test signin page and user existance

---
+++ tests/post_method_signpage_01/auth.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 4/4/2016
+ * Time: 10:54 μμ
+ */
+
+var_dump($_POST);
+
+// Get username, password from sign in form in src/sign.php:
+if (empty($_POST) === false) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    var_dump($username);
+    var_dump($password);
+}
+
+?>
---
+++ tests/user_exists_01/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 4/4/2016
+ * Time: 10:29 μμ
+ */
+
+include '../src/core/methods/users.php';
+include '../src/core/methods/general.php';
+
+if (user_exists("user") === true) {
+    echo 'exists';
+}
