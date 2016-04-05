1f163823197641d7edd3d50cb9e8c7c4a2d48559
karadalex <karadalex@gmail.com>
2016-04-05 17:57:12 +0300

    Defined configuration variables and added system execution script

--- src/core/sysconfig/setup_config.php
+++ src/core/sysconfig/setup_config.php
@@ -1,6 +1,12 @@
 <?php
-    // Get the configuration settings for the settings page
     if (empty($_POST) === false) {
+
+        /**************************************************************************
+        *
+        * Get All the Configuration Information from the Settings Page
+        *
+        **************************************************************************/
+
         // Application's Info
         $APP_HOSTNAME = $_POST['APP_HOSTNAME'];
         $APP_ADMIN_USERNAME = $_POST['APP_ADMIN_USERNAME'];
@@ -17,5 +23,18 @@
         // Raspberry Pi's Info
         $RASPBERRYPI_HOSTNAME = $_POST['RASPBERRYPI_HOSTNAME'];
         $RASPBERRYPI_PORT = $_POST['RASPBERRYPI_PORT'];
+
+
+
+
+        /**************************************************************************
+        *
+        * Export all the configuration information in config.php
+        *
+        **************************************************************************/
+
+        if ($APP_ADMIN_PASSWORD === $APP_ADMIN_PASSWORD_verification && $DB_ROOT_PASSWORD === $DB_ROOT_PASSWORD_verification) {
+            exec("python config_generator.py $APP_HOSTNAME $APP_ADMIN_USERNAME $APP_ADMIN_PASSWORD $DB_HOSTNAME $DB_PORT $DB_ROOT_USERNAME $DB_ROOT_PASSWORD $RASPBERRYPI_HOSTNAME $RASPBERRYPI_PORT");
+        }
     }
 ?>
