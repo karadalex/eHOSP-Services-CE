df38ba1ad2beeb845d6d9309b6c405b8f53e7e5c
Alex Karadimos <karadalex@gmail.com>
2016-04-05 19:34:09 +0300

    Use variables from config.php configuration file

--- src/core/db/connect.php
+++ src/core/db/connect.php
@@ -1,8 +1,10 @@
 <?php
-    $MYSQL_DATABASE_NAME = "ehosp";
-    $MYSQL_HOSTNAME = "localhost";
-    $MYSQL_USERNAME = "root";
-    $MYSQL_ROOT_PASSWORD = "rootpsswd";
+    $config = include('../../config.php');
+    
+    $MYSQL_DATABASE_NAME = $config['DB_NAME'];
+    $MYSQL_HOSTNAME = $config['DB_HOSTNAME'];
+    $MYSQL_USERNAME = $config['DB_ROOT_USERNAME'];
+    $MYSQL_ROOT_PASSWORD = $config['DB_ROOT_PASSWORD'];
 
     // For debugging purposes, not to be used in production:
     // mysqli_connect($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_ROOT_PASSWORD) or die(mysqli_error());
