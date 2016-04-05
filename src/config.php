20f6e2682b74d36790b06d9418b9f48d67d69748
karadalex <karadalex@gmail.com>
2016-04-05 17:55:06 +0300

    Dynamically created by python script and also added modification's datetime in docstring

--- src/config.php
+++ src/config.php
@@ -1,9 +1,12 @@
+
 <?php
     /***************************************************************************
     *
     * CONFIGURATION FILE:
     * Use this file to installand configure the application
     * @return : array with application's configuration information
+    *
+    * Last modified: 04/05/16 17:51:56
     *
     * How to use it in project:
     * $config = include('config.php');
@@ -12,20 +15,20 @@
     $CONFIG = array(
         // Application's Info
         'APP_NAME' => 'eHosp',
-        'APP_HOSTNAME' => 'localhost',
-        'APP_ADMIN_USERNAME' => 'admin',
-        'APP_ADMIN_PASSWORD' => 'adminpsswd',
+        'APP_HOSTNAME' => localhost,
+        'APP_ADMIN_USERNAME' => 'localhost',
+        'APP_ADMIN_PASSWORD' => 'admin',
 
         // Database's Info
         'DB_NAME' => 'ehosp',
-        'DB_HOSTNAME' => 'localhost',
-        'DB_PORT' => 3306,
-        'DB_ROOT_USERNAME' => 'root',
-        'DB_ROOT_PASSWORD' => 'rootpsswd',
+        'DB_HOSTNAME' => 'adminpsswd',
+        'DB_PORT' => localhost,
+        'DB_ROOT_USERNAME' => '3306',
+        'DB_ROOT_PASSWORD' => 'root',
 
         // Raspberry Pi's Info
-        'RASPBERRYPI_HOSTNAME' => '192.168.x.x',
-        'RASPBERRYPI_PORT' => 49153
+        'RASPBERRYPI_HOSTNAME' => 'rootpsswd',
+        'RASPBERRYPI_PORT' => localhost
     );
 
     return $CONFIG;
