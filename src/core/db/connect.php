3e80fb0dfd5aaa118bf4bfd56540e1e3833db9b6
unknown <up1046820@YK1-PC25.ece.upatras.gr>
2016-04-06 14:53:37 +0300

    Shortening code

--- src/core/db/connect.php
+++ src/core/db/connect.php
@@ -1,13 +1,6 @@
 <?php
     $config = include('../../config.php');
-    
-    $MYSQL_DATABASE_NAME = $config['DB_NAME'];
-    $MYSQL_HOSTNAME = $config['DB_HOSTNAME'];
-    $MYSQL_USERNAME = $config['DB_ROOT_USERNAME'];
-    $MYSQL_ROOT_PASSWORD = $config['DB_ROOT_PASSWORD'];
 
-    // For debugging purposes, not to be used in production:
-    // mysqli_connect($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_ROOT_PASSWORD) or die(mysqli_error());
-    mysqli_connect($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_ROOT_PASSWORD);
+    mysqli_connect($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
     mysqli_select_db($MYSQL_DATABASE_NAME);
 ?>
