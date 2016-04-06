42b75dcee863ed23af3dae3212f3f0f610d64197
Alex Karadimos <karadalex@gmail.com>
2016-04-07 02:14:00 +0300

    fix: Changed connection to MySQL by using mysqli PHP object

--- src/core/sysconfig/db_install.php
+++ src/core/sysconfig/db_install.php
@@ -2,16 +2,29 @@
     $config = include('../../config.php');
 
     // Connect to MySQL
-    $connect = mysql_connect($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
+    $connect = new mysqli($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
+
+    // Check connection
+    if ($connect->connect_error) {
+        die("Connection failed: " . $connect->connect_error ."<br>");
+    }
 
     // Create Database and insert the schema structure
-    $create_query = 'CREATE DATABASE ehosp';
-    $create_result = mysqli_query($create_query, $connect);
+    $create_query = "CREATE DATABASE IF NOT EXISTS `ehosp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+    if ($connect->query($create_query) === TRUE) {
+        echo "Database created successfully<br>";
+    } else {
+        echo "Error creating database: " . $connect->error ."<br>";
+    }
 
     // Load SQL code from file and import in MySQL Database
     $db_code_path = "../db/ehosp.sql";
     $db_code_file = fopen($db_code_path, "r") or die("Unable to find SQL file");
     $install_query = fread($db_code_file, filesize($db_code_path));
-    $install_result = mysqli_query($install_query, $connect);
+    if ($connect->multi_query($install_query) === TRUE) {
+        echo "Database installed successfully<br>";
+    } else {
+        echo "Error installing database: " . $connect->error ."<br>";
+    }
     fclose($db_code_file);
 ?>
