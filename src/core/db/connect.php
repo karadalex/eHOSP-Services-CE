70439d9f00fed30a69a25673d7f2caeb86030219
Alex Karadimos <karadalex@gmail.com>
2016-04-07 03:08:04 +0300

    Replaced procedural commands with Object-Oriented

--- src/core/db/connect.php
+++ src/core/db/connect.php
@@ -1,6 +1,11 @@
 <?php
     $config = include('../../config.php');
 
-    mysqli_connect($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
-    mysqli_select_db($MYSQL_DATABASE_NAME);
+    // Procedural:
+    // mysqli_connect($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
+    // mysqli_select_db($MYSQL_DATABASE_NAME);
+
+    // Object-Oriented:
+    $mysql_connect = new mysqli($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
+    $mysql_connect->select_db($config['DB_NAME']);
 ?>
