937457e200ee3b2aace4dd61eed11f3d0c6d35de
Alex Karadimos <karadalex@gmail.com>
2016-04-07 03:08:51 +0300

    Replaced procedural commands with Object-Oriented

--- tests/db_connect_01/index.php
+++ tests/db_connect_01/index.php
@@ -2,7 +2,7 @@
     chdir('../../src');
     $config = include('config.php');
 
-    $connection = mysqli_connect($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
+    $connection = new mysqli($config['DB_HOSTNAME'], $config['DB_ROOT_USERNAME'], $config['DB_ROOT_PASSWORD']);
     if (!$connection) {
         echo mysqli_error();
     }
