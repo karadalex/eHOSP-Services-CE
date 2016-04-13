f4388774f37a249f4ba102253a71f8ce4ff0f93a
unknown <up1046820@YK1-PC12.ece.upatras.gr>
2016-04-13 15:15:01 +0300

    Added Main Web apllication's Installation Page

---
+++ src/core/sysconfig/install.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<title>Simple Incision 1</title>
+    <link rel="stylesheet" href="../../css/simple_page.css" />
+    <?php $config = include('../../config.php'); ?>
+</head>
+<body>
+	<div id="container">
+		<div class="row">
+	        <h1>Install eHOSP Web Application</h1>
+	        <div id="text">
+                <p>
+					<a href="db_install.php" target="_blank">
+						Install Database
+					</a>
+					<a href="">
+						Install Application's Files
+					</a>
+                </p>
+            </div>
+      	</div>
+	</div>
+</body>
+</html>
