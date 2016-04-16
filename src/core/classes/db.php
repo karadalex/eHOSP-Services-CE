576bc766c1ca7d4faabb42e32ece124d0d72d4bb
Alex Karadimos <karadalex@gmail.com>
2016-04-16 21:30:02 +0300

    Created db class constructor

--- src/core/classes/db.php
+++ src/core/classes/db.php
@@ -1,4 +1,19 @@
 <?php
 class db {
-	
+	private static $_instance = null;
+	private $_pdo,
+	        $_query,
+	        $_error = false,
+	        $_results,
+	        $_count = 0;
+
+	// constructor
+	private function __construct() {
+		try {
+			// Connect to MySQL Server with info as specified in configuration file
+			$this->_pdo = new PDO('mysql:host='.config_get::get('DB_HOSTNAME').';dbname='.config_get::get('DB_NAME'), config_get::get('DB_ROOT_USERNAME'), config_get::get('DB_ROOT_PASSWORD'));
+		} catch(PDOException $e) {
+			die($e->getMessage());
+		}
+	}
 }
