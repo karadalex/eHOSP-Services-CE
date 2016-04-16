7adf2a3822cc57a4b14810d95d0cb1dc3e8366e9
Alex Karadimos <karadalex@gmail.com>
2016-04-16 11:30:21 +0300

    Added Technical details

--- src/config.php
+++ src/config.php
@@ -6,7 +6,7 @@
     * Use this file to installand configure the application
     * @return : array with application's configuration information
     *
-    * Last modified: Sun Apr 10 18:46:54 2016
+    * Last modified: Sat Apr 16 10:50:03 2016
     *
     * How to use it in project:
     * $config = include('config.php');
@@ -22,13 +22,22 @@
         // Database's Info
         'DB_NAME' => 'ehosp',
         'DB_HOSTNAME' => 'localhost',
-        'DB_PORT' => 3306,
+        'DB_PORT' => '3306',
         'DB_ROOT_USERNAME' => 'root',
         'DB_ROOT_PASSWORD' => 'rootpsswd',
 
         // Raspberry Pi's Info
         'RASPBERRYPI_HOSTNAME' => '192.168.1.2',
-        'RASPBERRYPI_PORT' => '80/ehosp-rasp/src'
+        'RASPBERRYPI_PORT' => '8080/hardware/src',
+
+        // Technical details
+        'remember' => array(
+            'cookie_name' => 'hash',
+            'cookie_expiry' => 604800
+        ),
+        'session' => array(
+            'session_name' => 'user'
+        )
     );
 
     return $CONFIG;
