af6fed9350a624d7e786805999a72233f7a7602f
Alex Karadimos <karadalex@gmail.com>
2016-04-16 21:28:34 +0300

    Updated dev test page

--- src/dev/tests.php
+++ src/dev/tests.php
@@ -2,8 +2,14 @@
 
 require_once '../core/init.php';
 
+echo "Testing Global variables<br>";
 var_dump($GLOBALS['config']);
 echo config_get::get('DB_ROOT_PASSWORD');
-echo "hi3";
+echo "<br>";
+echo "<br>";
+
+require_once '../core/db/connect.php';
+var_dump($mysql_connect);
+echo "hi";
 
 ?>
