9c9f089108a503ec06ec3ae59b3d42fee7546fd4
Alex Karadimos <karadalex@gmail.com>
2016-04-16 18:19:36 +0300

    More jQuery code to load dynamically lists which are interconnected

--- src/services/medical-diagnosis/js/diagnosis-form.js
+++ src/services/medical-diagnosis/js/diagnosis-form.js
@@ -1,13 +1,13 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "data/diagnosis.json";
+selection = "";
 
 // Parse JSON file and call selectList function
 xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         // Define Global variable holding JSON
         diagnosisJSON = JSON.parse(xmlhttp.responseText);
-        document.getElementById('diagnosis-form').innerHTML = '<h5>Please select a category below:</h5><br>';
-        selectList(diagnosisJSON, "pathologies-section");
+        selectList(diagnosisJSON, "pathologies-section", "diagnosis_form_1");
 
     }
 };
@@ -18,21 +18,48 @@ $(document).on('click', function() {
     $(document).ready(function() {
         $('#pathologies-section').change(function() {
             // Grab the value selected
-            var selection = this.value;
-            document.getElementById('diagnosis-form').innerHTML += '<h5>Please select a sub-category for ' + selection + ' below:</h5><br>';
-            selectList(diagnosisJSON["pathologies-section"], selection);
+            selection = this.value;
+            document.getElementById('diagnosis_form_2').innerHTML = '<h5>Please select a sub-category for ' + selection + ' below:</h5><br>';
+            document.getElementById('diagnosis_form_3').innerHTML = '';
+            selectList(diagnosisJSON["pathologies-section"], selection, "diagnosis_form_2");
         });
     });
 });
 
+$(document).on('click', function() {
+    $(document).ready(function() {
+        $('#'+selection.replace(/\s+/g, '')).change(function() {
+            // Grab the value selected
+            var new_selection = this.value;
+            document.getElementById('diagnosis_form_3').innerHTML = '<h5>Please select a sub-category for ' + new_selection + ' below:</h5><br>';
+            selectListArray(diagnosisJSON["pathologies-section"][selection], new_selection, "diagnosis_form_3");
+            document.getElementById('diagnosis_form_3').innerHTML += "<button id=\"SubmitDiagnosis\" type=\"button\" class=\"services\">Submit Diagnosis </button>";
+        });
+    });
+});
+
+
 // Creates an HTML select list, using <select> tag, with id be the primaryKey
-function selectList(json, primaryKey) {
-	var output = '<select id="' + primaryKey + '">';
+function selectList(json, primaryKey, div_id) {
+	var output = '<select id="' + primaryKey.replace(/\s+/g, '') + '">';
 	var object = json[primaryKey];
+    output += '<option>Click to select</option>';
 	for (key in object) {
 		output += '<option ' + 'value=' + '"' + key + '">' + key + '</option>';
 	}
 	output += '</select>';
-	var update = document.getElementById('diagnosis-form');
+	var update = document.getElementById(div_id);
 	update.innerHTML += output;
+}
+
+function selectListArray(json, primaryKey, div_id) {
+    var output = '<select id="' + primaryKey.replace(/\s+/g, '') + '">';
+    var object = json[primaryKey];
+    output += '<option>Click to select</option>';
+    for (i =0; i<object.length; i++) {
+        output += '<option ' + 'value=' + '"' + object[i] + '">' + object[i] + '</option>';
+    }
+    output += '</select>';
+    var update = document.getElementById(div_id);
+    update.innerHTML += output;
 }
