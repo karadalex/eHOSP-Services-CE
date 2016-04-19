508a3ca25aa79d157a93edd5d148bd1ad2d887ee
Alex Karadimos <karadalex@gmail.com>
2016-04-19 23:24:00 +0300

    Added buttons to control map and added stylesheet

--- src/services/med-gis/maps.php
+++ src/services/med-gis/maps.php
@@ -6,6 +6,7 @@
 		include '../includes/html/head.html';
 	?>
 	<link rel="stylesheet" href="leaflet/leaflet.css" />
+	<link rel="stylesheet" href="css/edit_map.css" />
 </head>
 <body>
 	<div id="container">
@@ -25,6 +26,26 @@
 	<script src="leaflet/leaflet.js"></script>
 	<script src="js/main.js"></script>
 	<script src="js/gis_remote_nodes.js"></script>
+
+	<div id="edit_map">
+		<ul>
+			<li>
+				<button onclick="gis_remote_nodes();">
+					Candidate remote places
+				</button>
+			</li>
+			<li>
+				<button>
+					Remote places with working surgery robots
+				</button>
+			</li>
+			<li>
+				<button>
+					Hospitals or Doctors in charge
+				</button>
+			</li>
+		</ul>
+	</div>
 
 	<script src="../../js/foundation.min.js"></script>
     <script>
