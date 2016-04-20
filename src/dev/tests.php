07f6e940baf5deb3cdbd63bf006976d05947672e
unknown <up1046820@YK1-PC12.ece.upatras.gr>
2016-04-20 14:28:37 +0300

    Added comments to separate different tests and added test for query and results

--- src/dev/tests.php
+++ src/dev/tests.php
@@ -7,7 +7,11 @@
 
 require_once '../core/init.php';
 
-// Testing global variables
+
+/*********************************************************************************
+* Testing global variables
+*
+*********************************************************************************/
 echo "Testing Global variables<br>";
 var_dump($GLOBALS['config']);
 echo config_get::get('DB_ROOT_PASSWORD');
@@ -20,24 +24,71 @@ var_dump(config_get::get('DB_ROOT_USERNAME'));
 echo "<br>";
 echo "<br>";
 
-// Testing ../core/db/connect.php
+
+/*********************************************************************************
+* Testing ../core/db/connect.php
+*
+*********************************************************************************/
 // echo "Testing ../core/db/connect.php<br>";
 // require_once '../core/db/connect.php';
 // var_dump($mysql_connect);
 echo "<br>";
 echo "<br>";
 
-// Testing db class 
+
+/*********************************************************************************
+* Testing db class
+*
+*********************************************************************************/
 // Test if it connects and whether it connects once
 DB::getInstance();
 DB::getInstance();
 
-// Testing db query
+
+/*********************************************************************************
+* Testing db query
+*
+*********************************************************************************/
 // $user1 must return 'Users were found!'
 $user1 = DB::getInstance()->query("SELECT username FROM user_registry");
 if ($user1->error()) {
-	echo 'No user found!';
+	echo 'No user found!<br>';
 } else {
-	echo 'Users were found!';
+	echo 'Users were found!<br>';
 }
 
+
+/*********************************************************************************
+* Testing get method of class
+*
+*********************************************************************************/
+// $user2 must return Users were found!
+$user2 = DB::getInstance()->get('user_registry', array('username', '=', 'alex'));
+if (!$user2->count()) {
+	echo 'No user found!<br>';
+} else {
+	echo 'Users were found!<br>';
+}
+// $user3 must return No user found!
+$user3 = DB::getInstance()->get('user_registry', array('username', '=', 'billy'));
+if (!$user3->count()) {
+	echo 'No user found!<br>';
+} else {
+	echo 'Users were found!<br>';
+}
+
+
+
+/*********************************************************************************
+* Testing results method of class; print results from query
+*
+*********************************************************************************/
+$user4 = DB::getInstance()->get('user_registry', array('username', '=', 'alex'));
+if (!$user4->count()) {
+	echo 'No user found!<br>';
+} else {
+	foreach($user4->results() as $user) {
+		echo $user->username, " ", $user->password, "<br>";
+	}
+}
+echo "hi6";
