9f114a97e9e59144c34627e53630f97809b64d30
Alex <karadalex@gmail.com>
2016-04-27 14:00:26 +0300

    Added validation rules and javascript script to output the errors

--- src/dev/test-form-input.php
+++ src/dev/test-form-input.php
@@ -1,7 +1,39 @@
-<?php 
+<?php
 require_once '../core/init.php';
 if(Input::exists()) {
-	echo Input::get('username');
+	// echo Input::get('username');
+
+	$validate = new Validation();
+	$validation = $validate->check($_POST, array(
+		'username' => array(
+			'required' => true,
+			'min' => 4,
+			'max' => 20,
+			'unique' => 'user_registry'
+		),
+		'password' => array(
+			'required' => true,
+			'min' => 6
+		),
+		'password_again' => array(
+			'required' => true,
+			'matches' => 'password'
+		),
+		// 'name' => array(
+		// 	'required' => true,
+		// 	'min' => 2,
+		// 	'max' => 50
+		// )
+	));
+
+	if ($validation->passed()) {
+		echo 'Passed';
+	} else {
+		$error_msg = "";
+		foreach($validation->errors() as $error) {
+			$error_msg .= $error . '\n';
+		}
+	}
 }
 ?>
 
@@ -11,6 +43,15 @@ if(Input::exists()) {
 *
 *********************************************************************************/
 <form action="" method="post">
-	<input type="text" name="username">
+	<input type="text" name="username" value="<?php echo escape(Input::get('username')); ?>">
+	<input type="password" name="password" value="">
+	<input type="password" name="password_again" value="">
 	<input type="submit" value="Register">
 </form>
+
+
+<?php
+// Output error Message when page is loaded
+if (strlen($error_msg) > 0) {
+	echo '<script type="text/javascript">alert("' . $error_msg . '");</script>';
+} ?>
