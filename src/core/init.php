55e4eda3816557bc69d7c24b04ba0ceb9e71162e
Alex Karadimos <karadalex@gmail.com>
2016-05-03 01:39:24 +0300

    Added sessio_start()command and fixed path for config.php inclusion

--- src/core/init.php
+++ src/core/init.php
@@ -1,11 +1,19 @@
 <?php
-    $app_config = include('../config.php');
-    $GLOBALS['errors'] = array();
-    $GLOBALS['config'] = $app_config;
+session_start();
 
-    // Autoload required php files from classes folder
-    spl_autoload_register(function($class) {
-        require_once 'classes/' . $class . '.php';
-    });
+chdir(dirname(__FILE__));
+chdir('../');
+$config_path = getcwd() . '/config.php';
+$app_config = include($config_path);
 
-    require_once 'methods/sanitize.php';
+$GLOBALS['errors'] = array();
+$GLOBALS['config'] = $app_config;
+
+// Autoload required php files from classes folder
+spl_autoload_register(function($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'methods/sanitize.php';
+
+?>
