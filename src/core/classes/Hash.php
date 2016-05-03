2c8417f4c5e118869105cddd7b7629be858634c8
Alex Karadimos <karadalex@gmail.com>
2016-05-03 14:09:29 +0300

    Replaced buggy mcrypt_create_iv method with a custom random string generator algorithm.

--- src/core/classes/Hash.php
+++ src/core/classes/Hash.php
@@ -1,13 +1,20 @@
 <?php
 
 class Hash {
-
+    
     public static function make($string, $salt = '') {
         return hash('sha256', $string . $salt);
     }
 
     public static function salt($length) {
-        return mcrypt_create_iv($length);
+        $result = "";
+        $chars = "abcdefghijklmnopqrstuvwxyz_@#$%^&*()+={}~`?!-0123456789";
+        $charArray = str_split($chars);
+        for($i = 0; $i < $length; $i++){
+    	    $randItem = array_rand($charArray);
+    	    $result .= "".$charArray[$randItem];
+        }
+        return $result;
     }
 
     public static function unique() {
