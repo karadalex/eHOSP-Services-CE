cdb7a2435dabfb24c812ac12e74cd027e4d9d9f9
Alex <karadalex@gmail.com>
2016-05-06 12:52:45 +0300

    Added functionality for Remember Me Cookie

--- src/core/init.php
+++ src/core/init.php
@@ -16,4 +16,13 @@ spl_autoload_register(function($class) {
 
 require_once 'methods/sanitize.php';
 
-?>
+
+if (Cookie::exists(config_get::get('remember/cookie_name')) && !Session::exists(config_get::get('session/session_name'))) {
+    $hash = Cookie::get(Config::get('remember/cookie_name'));
+    $hashCheck = DB::getInstance()->get('user_session', array('hash', '=', $hash));
+    
+    if ($hashCheck->count()) {
+        $user = new User($hashCheck->first()->user_id);
+        $user->login();
+    }
+}
