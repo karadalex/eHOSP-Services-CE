39b12290fd4dcdce7e261113dcff9ccdea2a5d87
Alex Karadimos <karadalex@gmail.com>
2016-05-08 19:59:31 +0300

    Fixing sql syntax bug in DELETE

--- src/core/classes/DB.php
+++ src/core/classes/DB.php
@@ -79,7 +79,7 @@ class DB {
 	}
 
 	public function delete($table, $where) {
-		return $this->action('DELETE *', $table, $where);
+		return $this->action('DELETE', $table, $where);
 	}
 
 	public function insert($table, $fields = array()) {
