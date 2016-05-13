a400ed9861e9ebb257bd0cf234b2c94e5f261eeb
Alex <karadalex@gmail.com>
2016-05-13 19:48:40 +0300

    Initialized Body Measurements pages

---
+++ src/services/patient-med-profile/measurement_includes/BodyMeasurements/bodyfat.php
@@ -0,0 +1,28 @@
+<h1> Body Fat Percentage </h1>
+
+<div class="StatsticsIframe">
+    <iframe src="loadStat.php" frameborder="0" allowfullscreen=""></iframe>
+</div>
+
+
+<p>
+    The body fat percentage (BFP) of a human or other living being is the total
+    mass of fat divided by total body mass; body fat includes essential body fat
+    and storage body fat. Essential body fat is necessary to maintain life and
+    reproductive functions. The percentage of essential body fat for women is
+    greater than that for men, due to the demands of childbearing and other
+    hormonal functions. The percentage of essential fat is 3–5% in men, and 8–12%
+    in women (referenced through NASM). Storage body fat consists of fat
+    accumulation in adipose tissue, part of which protects internal organs in
+    the chest and abdomen. The minimum recommended total body fat percentage
+    exceeds the essential fat percentage value reported above. A number of
+    methods are available for determining body fat percentage, such as
+    measurement with calipers or through the use of bioelectrical impedance analysis.
+
+    <br>
+    <i><a href="https://en.wikipedia.org/wiki/Body_fat_percentage">Wikipedia</a></i>
+</p>
+
+<button type="button" class="services" onclick="var name = 'Body Measurements'; Start(Main, name, name);">
+    Go Back
+</button>
