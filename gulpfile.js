349dc7e4bd635cb8ed3f7778c613c354811c963e
Alex Karadimos <karadalex@gmail.com>
2016-05-14 03:37:35 +0300

    Added plumber to handle erros

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    css_minify = require('gulp-clean-css');
+    css_minify = require('gulp-clean-css'),
+    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber');
 
 var services = ['medical-diagnosis', 'patient-med-profile', 'research-platform', 'surgery-printing', 'genetic-code', 'med-gis'];
 
@@ -9,25 +11,28 @@ var services = ['medical-diagnosis', 'patient-med-profile', 'research-platform',
 // Minify javascript files and put the minified version in the dist folder
 gulp.task('js-minify', function(){
     gulp.src('src/js/**')
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 
     // Leaflet vendor
     gulp.src('src/vendor/leaflet/leaflet.js')
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(gulp.dest('dist/vendor/leaflet/leaflet.js'));
-    }
 });
 
 // Image task
 // Compress images files
 gulp.task('img-minify', function(){
     gulp.src('src/media/img/**')
+        .pipe(plumber())
         .pipe(imagemin())
         .pipe(gulp.dest('dist/media/img'));
 
     // Leaflet vendor
     gulp.src('src/vendor/leaflet/images/**')
+        .pipe(plumber())
         .pipe(imagemin())
         .pipe(gulp.dest('dist/vendor/leaflet/images/'));
 });
@@ -36,12 +41,14 @@ gulp.task('img-minify', function(){
 // Minify CSS code
 gulp.task('minify-css', function() {
     gulp.src('src/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(plumber())
+        .pipe(css_minify({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/css'));
 
     // Leaflet vendor
     gulp.src('src/vendor/leaflet/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(plumber())
+        .pipe(css_minify({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/vendor/leaflet'));
 });
 
@@ -49,16 +56,22 @@ gulp.task('minify-css', function() {
 // Simple copy content to dist
 gulp.task('simply-copy', function(){
     gulp.src('src/core/**')
+        .pipe(plumber())
         .pipe(gulp.dest('dist/core'));
     gulp.src('src/includes/**')
+        .pipe(plumber())
         .pipe(gulp.dest('dist/includes'));
     gulp.src('src/services/**')
+        .pipe(plumber())
         .pipe(gulp.dest('dist/services'));
     gulp.src('src/*.php')
+        .pipe(plumber())
         .pipe(gulp.dest('dist'));
     gulp.src('src/*.txt')
+        .pipe(plumber())
         .pipe(gulp.dest('dist'));
     gulp.src('src/*.ico')
+        .pipe(plumber())
         .pipe(gulp.dest('dist'));
 });
 
