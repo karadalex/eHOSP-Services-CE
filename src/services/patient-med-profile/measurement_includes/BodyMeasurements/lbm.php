af6645ddc7c5d57904a41f6c9e77db6fc9fecab1
unknown <up1046820@YK1-PC16.ece.upatras.gr>
2016-05-19 16:46:05 +0300

    Added basic template without info

--- src/services/patient-med-profile/measurement_includes/BodyMeasurements/lbm.php
+++ src/services/patient-med-profile/measurement_includes/BodyMeasurements/lbm.php
@@ -1,5 +1,99 @@
-<h1> Lean Body Mass </h1>
+<?php
+require_once '../../../../core/init.php';
+chdir(dirname(__FILE__));
+?>
 
-<button type="button" class="services" onclick="var name = 'Body Measurements'; Start(Main, name, name);">
-    Go Back
-</button>
+
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<title>Patient Health</title>
+	<?php
+		include '../../../../includes/html/head.html';
+	?>
+	<link rel="stylesheet" type="text/css" href="css/blue-buttons-list.css">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          backgroundColor: '#ebebeb'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+
+      $(window).resize(function(){
+        drawChart();
+      });
+    </script>
+</head>
+<body>
+	<div id="container">
+		<?php
+			include '../../../../includes/php/header.php';
+		?>
+
+
+		<?php
+        if(Session::exists('home')) {
+            echo '<p>' . Session::flash('home'). '</p>';
+        }
+        $user = new User(); //Current
+        if($user->isLoggedIn()) {
+        ?>
+
+        <div class="row">
+            <h1> Lean Body Mass </h1>
+
+            <div id="curve_chart" style="width: 100%; height: 500px"></div>
+
+            <p>
+
+                <br>
+                <i><a href="">Wikipedia</a></i>
+            </p>
+        </div>
+
+
+      	<?php
+        } else {
+        ?>
+        <div class="row">
+        	<h1>
+	        	Error
+	        </h1>
+        	<p>
+        		You are currently not Signed In!<br>
+        		Please <a href='sign.php'>Sign In</a> or <a href='register.php'>Register</a>
+        	</p>
+        </div>
+        <?php
+        }
+        ?>
+
+	</div>
+
+	<script src="js/foundation.min.js"></script>
+    <script>
+    	$(document).foundation();
+    </script>
+</body>
+</html>
