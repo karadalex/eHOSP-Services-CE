3f842eeb8c3729d4f93dff211e0ec9e44a998af1
unknown <up1046820@YK1-PC16.ece.upatras.gr>
2016-05-20 14:30:17 +0300

    Added APP_PORT

--- src/config.php
+++ src/config.php
@@ -15,7 +15,8 @@
     $CONFIG = array(
         // Application's Info
         'APP_NAME' => 'eHosp',
-        'APP_HOSTNAME' => '127.0.0.1',
+        'APP_HOSTNAME' => 'localhost',
+        'APP_PORT' => '80',
         'APP_ADMIN_USERNAME' => 'admin',
         'APP_ADMIN_PASSWORD' => 'adminpsswd',
 
@@ -24,7 +25,7 @@
         'DB_HOSTNAME' => '127.0.0.1',
         'DB_PORT' => '3306',
         'DB_ROOT_USERNAME' => 'root',
-        'DB_ROOT_PASSWORD' => 'rootpsswd',
+        'DB_ROOT_PASSWORD' => '',
 
         // Raspberry Pi's Info
         'RASPBERRYPI_HOSTNAME' => '192.168.1.2',
