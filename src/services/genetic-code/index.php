4d8ecb20f40003561a1d090c055d94e409c5b5a6
unknown <up1046820@YK1-PC16.ece.upatras.gr>
2016-05-20 14:31:56 +0300

    Added info about what is genetic code

--- src/services/genetic-code/index.php
+++ src/services/genetic-code/index.php
@@ -35,6 +35,15 @@ chdir(dirname(__FILE__));
 	        <p>
 				A service for analyzing genetic code and use it for customized treatments
 			</p>
+
+			<h2> What is Genetic Code? </h2>
+			<p>
+				The genetic code is the set of rules by which information encoded in
+				genetic material (DNA or RNA sequences) is translated into proteins
+				(amino acid sequences) by living cells.
+				<br>
+				<i><a href="https://www.sciencedaily.com/terms/genetic_code.htm">Science Daily</a></i>
+			</p>
 			<ul id="buttons">
 
 			</ul>
