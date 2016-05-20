4396e23e1659c4cb861e04bbe6449b07e3186018
unknown <up1046820@YK1-PC16.ece.upatras.gr>
2016-05-20 14:29:09 +0300

    Included information about the platform, taken from research paper

--- src/about.php
+++ src/about.php
@@ -24,6 +24,25 @@ require_once 'core/init.php';
 				Welcome to eHOSP, the hospital on the cloud,
 				a platform for e-health and e-medicine!
 			</p>
+			<p>
+				In its core, this platform is a hospital that
+				runs in the Cloud and executes physical tasks with Robotics.
+				<br>
+				More specifically, the aim of this platform is to create a smart
+				cross-platform Internet of Things designated to serve e-health and
+				e-medicine applications as well as remote control of a 3D bioprinter-surgery
+				robotic tool and also investigate how such a robotic platform can be designed
+				and manufactured. Cross-platform refers to the ability of running the
+				infrastructure in various operating systems, virtual machines, bare-metal
+				servers and container ecosystems and also the ability to have access to
+				these applications through any web browser, operating system, tablet or
+				smartphone. E-health and e-medicine refer to applications on the Internet
+				(cloud) to monitor a patient’s health and/or diagnose from his symptoms remotely
+				with or without the presence of an actual doctor and also making medical research.
+				Moreover, in this platform research was done about how robotic surgery procedures
+				and 3D medical bioprinting can be performed simultaneously through the Internet
+				and IoT infrastructure.
+			</p>
       	</div>
 	</div>
 
