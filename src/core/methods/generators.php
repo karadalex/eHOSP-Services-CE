a82044f8a917b092df47e2b51a165fe889a0210d
Alex <karadalex@gmail.com>
2016-05-24 22:30:57 +0300

    Php methods to generate html code and avoid DRY code

---
+++ src/core/methods/generators.php
@@ -0,0 +1,27 @@
+<?php
+
+function button($button_url = null, $button_text) {
+    if (isset($button_url)) {
+        $btn_html = "
+        <a href=\"$button_url\" class=\"no-menu\">
+            <button type=\"button\" class=\"services\">
+                $button_text
+            </button>
+        </a>
+        ";
+    }
+
+    return $btn_html;
+}
+
+function li($html_content) {
+    if (isset($html_content)) {
+        $li_html = "
+        <li>
+            $html_content
+        </li>
+        ";
+    }
+
+    return $li_html;
+}
