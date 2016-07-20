d6515b775fa9f4d9dd3e18465a879b08103d13b2
Alex Karadimos <karadalex@gmail.com>
2016-07-20 16:23:16 +0300

    Added post operative treatments service

--- src/views/surgeon.php
+++ src/views/surgeon.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_URI'] == "/server/src/views/doctor.php") {
     echo li(button("services/med-gis", "Medical Geographic Information System"));
     echo li(button("services/research-platform", "Research Platform"));
     echo li(button("services/surgery-printing", "Remote Surgery & 3D Bioprinting tool"));
-    echo li(button("services/research-platform", "Post-operative Treatments"));
+    echo li(button("services/postop-treatments", "Post-operative Treatments"));
     echo li(button("services/edu", "Educational Platform"));
     echo li(button("services/communication", "Communication"));
 }
