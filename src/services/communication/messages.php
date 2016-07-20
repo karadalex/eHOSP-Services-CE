5a45c8cb10c6a4e0f54d5455abec75516f3bb2fd
Alex Karadimos <karadalex@gmail.com>
2016-07-20 16:22:08 +0300

    INitial Commit

---
+++ src/services/communication/messages.php
@@ -0,0 +1,63 @@
+<?php
+require_once '../../core/init.php';
+chdir(dirname(__FILE__));
+?>
+
+
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<title>Messages</title>
+	<?php
+		include '../../includes/php/head.php';
+	?>
+</head>
+<body>
+	<div id="container">
+		<?php
+			include '../../includes/php/header.php';
+		?>
+
+		<?php
+        if(Session::exists('home')) {
+            echo '<p>' . Session::flash('home'). '</p>';
+        }
+        $user = new User(); //Current
+        if($user->isLoggedIn()) {
+        ?>
+
+
+		<div class="row">
+	        <h1>Messages</h1>
+	        <br>
+
+			
+      	</div>
+
+      	<?php
+        } else {
+        ?>
+        <div class="row">
+        	<h1>
+	        	Error
+	        </h1>
+        	<p>
+        		You are currently not Signed In!<br>
+        		Please <a href='sign.php'>Sign In</a> or <a href='register.php'>Register</a>
+        	</p>
+        </div>
+        <?php
+        }
+        ?>
+
+	</div>
+
+	<?php include '../../includes/php/footer.php'; ?>
+
+	<script src="js/foundation.min.js"></script>
+    <script>
+    	$(document).foundation();
+    </script>
+</body>
+</html>
