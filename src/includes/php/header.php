b8bb5f5d57c6d5a37efc57475fd59674ff570516
Alex Karadimos <karadalex@gmail.com>
2016-07-20 16:05:59 +0300

    Added search bar and improved styles

--- src/includes/php/header.php
+++ src/includes/php/header.php
@@ -1,7 +1,24 @@
 <style>
+.header-row {
+  width: 100%;
+}
+
+.menu {
+  display: block;
+}
+
+.mobile-only, #mobile {
+  display: none;
+}
+
+ul#menu {
+  margin-left: 0px;
+}
+
 .dropdown {
     position: relative;
     display: inline-block;
+    margin-right: 10px;
 }
 
 .dropdown-content {
@@ -26,7 +43,53 @@
 	display: none;
 }
 
-@media only screen and (max-width: 40.063em) {
+.search {
+  display: inline-block;
+  position: relative;
+  top: 5px;
+  left: 0px;
+  margin: auto;
+  width: 180;
+}
+
+.search input[type=text] {
+  width: 130px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-image: url('media/searchicon.png');
+  background-position: 10px 10px;
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+}
+
+.search input[type=text]:focus {
+  width: 180px;
+}
+
+@media only screen and (max-width: 64.063em) {
+  .search {
+    display: inline-block;
+    position: relative;
+    top: 0px;
+    left: 0px;
+    height: 90%;
+  }
+}
+
+@media only screen and (max-width: 44.063em) {
+  .menu {
+    display: none;
+  }
+
+  .mobile-only {
+    display: block;
+  }
+
 	.dropdown-mobile-content {
 		display: block;
 	}
@@ -34,6 +97,16 @@
 	.dropdown {
 		display: none;
 	}
+
+  .search input[type=text] {
+    width: 80%;
+    position: relative;
+    top: 15px;
+  }
+
+  .search input[type=text]:focus {
+    width: 100%;
+  }
 
 	@media only screen and (max-height: 360px) {
 		hr.dropdown-mobile-content {
@@ -46,32 +119,26 @@
 
 <header>
 	<a id="toggleMenu"><img src="media/img/menu.png" /></a>
-	<div class="row">
+
+  <!-- Header for bigger devices -->
+	<div class="row header-row">
 		<ul>
 			<li><img src="media/img/LOGO_big.png" /></li>
 			<li>
-				<ul id="menu">
-					<li id="menuOut"><img src="media/img/back.png" /></li>
+				<ul id="menu" class="menu">
 					<li><a class="menu" href="index.php">Home</a></li>
 					<li><a class="menu" href="about.php">About</a></li>
 					<li><a class="menu" href="doc.php">Doc</a></li>
 					<li><a class="menu" href="contact.php">Contact</a></li>
 
-
 					<?php
-			        if(Session::exists('home')) {
-			            echo '<p>' . Session::flash('home'). '</p>';
-			        }
-			        $user = new User(); //Current
-			        if($user->isLoggedIn()) {
-			        ?>
+	        // if(Session::exists('home')) {
+	        //     echo '<p>' . Session::flash('home'). '</p>';
+	        // }
+	        $user = new User(); //Current
+	        if($user->isLoggedIn()) {
+	        ?>
 
-					<hr class="dropdown-mobile-content">
-					<li class="dropdown-mobile-content"><a class="menu" href="profile.php">My Profile</a></li>
-					<li class="dropdown-mobile-content"><a class="menu" href="user_settings.php">Settings</a></li>
-					<li class="dropdown-mobile-content"><a class="menu" href="signout.php">Logout</a></li>
-
-					<li></li>
 					<div class="dropdown">
 						<a class="dropdown">Me</a>
 						<div class="dropdown-content">
@@ -81,8 +148,49 @@
 						</div>
 					</div>
 					<?php } ?>
+          <div class="search">
+            <form>
+              <input type="text" name="search" placeholder="Search..">
+            </form>
+          </div>
 				</ul>
 			</li>
 		</ul>
 	</div>
+
+  <!-- Header/Side Bar for small devices -->
+  <div class="row header-row">
+    <ul>
+      <li>
+        <ul id="mobile" class="mobile-only">
+          <li id="menuOut"><img src="media/img/back.png" /></li>
+          <li><a class="mobile-only" href="index.php">Home</a></li>
+          <li><a class="mobile-only" href="about.php">About</a></li>
+          <li><a class="mobile-only" href="doc.php">Doc</a></li>
+          <li><a class="mobile-only" href="contact.php">Contact</a></li>
+
+
+          <?php
+          // if(Session::exists('home')) {
+          //     echo '<p>' . Session::flash('home'). '</p>';
+          // }
+          $user = new User(); //Current
+          if($user->isLoggedIn()) {
+          ?>
+
+          <hr class="dropdown-mobile-content">
+          <li class="dropdown-mobile-content"><a class="mobile-only" href="profile.php">My Profile</a></li>
+          <li class="dropdown-mobile-content"><a class="mobile-only" href="user_settings.php">Settings</a></li>
+          <li class="dropdown-mobile-content"><a class="mobile-only" href="signout.php">Logout</a></li>
+
+          <?php } ?>
+          <div class="search center">
+            <form>
+              <input type="text" name="search" placeholder="Search..">
+            </form>
+          </div>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </header>
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
 	$("#toggleMenu").on('click', function() {
-		$(".row ul li ul").toggle('slide', {direction: "right"}, 400);
+		$("#mobile").toggle('slide', {direction: "right"}, 400);
 	});
 
 	$("#menuOut").on('click', function() {
-		$(".row ul li ul").toggle('slide', {direction: "right"}, 400);
+		$("#mobile").toggle('slide', {direction: "right"}, 400);
 	});
 
 });
