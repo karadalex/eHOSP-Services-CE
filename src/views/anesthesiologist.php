debacac527118591eac2fda4cb3aab7b4f258a95
Alex Karadimos <karadalex@gmail.com>
2016-07-20 16:23:01 +0300

    Added patients

--- src/views/anesthesiologist.php
+++ src/views/anesthesiologist.php
@@ -3,6 +3,7 @@
 if ($_SERVER['REQUEST_URI'] == "/server/src/views/anesthesiologist.php") {
     http_response_code(404);
 } else {
+	echo li(button("services/patients", "Patients"));
     echo li(button("services/edu", "Educational Platform"));
     echo li(button("services/communication", "Communication"));
 }
