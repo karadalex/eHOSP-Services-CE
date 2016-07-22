2e40db934b486c96e32e0d9a8f269319b776207d
Alex Karadimos <karadalex@gmail.com>
2016-07-22 12:46:09 +0300

    Added documentation

--- src/includes/php/footer.php
+++ src/includes/php/footer.php
@@ -62,7 +62,7 @@
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="about.php">About</a></li>
-                    <li><a href="#">Documentation</a></li>
+                    <li><a href="doc.php">Documentation</a></li>
                     <li><a href="#">Blog</a></li>
                 </ul>
             </div>
