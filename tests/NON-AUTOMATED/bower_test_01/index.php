e412036dda4134b5ed4bcd4b37359a01d96d4043
Alex Karadimos <karadalex@gmail.com>
2016-07-22 20:19:27 +0300

    bower poolymer test

---
+++ tests/NON-AUTOMATED/bower_test_01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bower Test 1</title>
+	<script src="../../../src/vendor/bower/webcomponentsjs/webcomponents.js"></script>
+	<link rel="import" href="../../../src/vendor/bower/paper-ripple/paper-ripple.html">
+</head>
+<body>
+	<paper-ripple></paper-ripple>
+	<div style="height: 100px; width: 100%"></div>
+
+</body>
+</html>
