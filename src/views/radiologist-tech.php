a20ae160cb4112f6344416073b3989327c56a6b2
Alex Karadimos <karadalex@gmail.com>
2016-07-22 12:43:07 +0300

    Added radiology lab

--- src/views/radiologist-tech.php
+++ src/views/radiologist-tech.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_URI'] == "/server/src/views/doctor.php") {
     http_response_code(404);
 } else {
     echo li(button("services/patients/", "Patients"));
-    echo li(button("services/", "Imaging Procedures"));
+    echo li(button("services/radiology-lab", "Radiology Lab"));    
     echo li(button("services/edu", "Educational Platform"));
     echo li(button("services/communication", "Communication"));
 }
