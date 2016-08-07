4941f226461fb116742d6c5bf9b05efe7b9ca14a
Alex Karadimos <karadalex@gmail.com>
2016-08-07 13:41:58 +0300

    Added nodejs script to handle Dockerfiles and Docker container builds

---
+++ docker.js
@@ -0,0 +1,9 @@
+/****************************************************************
+* Javascript Shell-like script to install one of Docker solutions
+* Author: Alex Karadimos
+*
+*****************************************************************/
+
+
+require('shelljs/global');
+var chalk = require('chalk');
