ce6a9310ff747e2080abe7990fa23b67e5a07f2e
Alex Karadimos <karadalex@gmail.com>
2016-08-22 15:49:56 +0300

    API Routes version1 initial commit - Simple controllers and routes with user authentication, TODO: Change the authentication from User Auth to API Auth (use tokens)

--- app/Http/ApiRoutes.php
+++ app/Http/ApiRoutes.php
@@ -1,14 +1,19 @@
 <?php
 
+Route::get('/api', function () {
+    return 'eHOSP API';
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
-|  API methods
+|  API methods (v1)
+|  version: 1
 |
 */
 
-Route::get('/api', function () {
-    return 'API Test';
-});
+Route::resource('/api/v1/users', 'API\v1\UserApi');
+Route::resource('/api/v1/gis-nodes', 'API\v1\MedGis');
+Route::resource('/api/v1/emergency-contacts', 'API\v1\EmergencyContacts');
---
+++ app/Http/Controllers/API/v1/EmergencyContacts.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace eHOSP\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+
+use eHOSP\Http\Requests;
+use eHOSP\Http\Controllers\Controller;
+
+use eHOSP\Models\EmergencyContacts as Contacts;
+
+class EmergencyContacts extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $all_contacts = Contacts::all();
+        return response()->json($all_contacts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contacts::find($id);
+        return response()->json($contact);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
