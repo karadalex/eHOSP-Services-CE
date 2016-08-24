8201914fa1b317a12da1590c54ed4d7986a9dcab
Alex Karadimos <karadalex@gmail.com>
2016-08-24 15:45:03 +0300

    Translated Services Titles

---
+++ resources/lang/GRC/services.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in displaying navigation
+    | (e.g. Home etc.). You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    '3D Bioprinting' => '3Δ Εκτύπωση',
+    'Accounting Analytics' => 'Accounting Analytics',
+    'BPM' => 'BPM',
+    'Business Intelligence' => 'Business Intelligence',
+    'Communication' => 'Επικοινωνία',
+    'Collaboration' => 'Collaboration',
+    'CRM' => 'CRM',
+    'Diseases Diagnosis' => 'Διάγνωση Ασθενειών',
+    'Document Management' => 'Document Management',
+    'Educational Platform' => 'Εκπαιδευτική Πλατφόρμα',
+    'Email' => 'Email',
+    'Emergency' => 'Έκτακτη Ανάγκη',
+    'ERP' => 'ERP',
+    'First Aid' => 'Πρώτες Βοήθειες',
+    'Genetic Code Service' => 'Υπηρεσία Γενετικού Κώδικα',
+    'Healthcare Policies' => 'Healthcare Policies',
+    'Healthcare Servicies' =>'Healthcare Servicies',
+    'Human Resources' => 'Human Resources',
+    'Medical G.I.S.' => 'Medical G.I.S.',
+    'Medical Reports' => 'Medical Reports',
+    'Medical Lab' => 'Ιατρικό Εργαστήριο',
+    'Marketing Automation' => 'Marketing Automation',
+    'Patients' => 'Ασθενείς',
+    'Patient Health Profile' => 'Patient Health Profile',
+    'Patient Monitoring' => 'Patient Monitoring',
+    'Pharmacy Lab' => 'Φαρμακευτικό Εργαστήριο',
+    'Physical Treatments' => 'Physical Treatments',
+    'Prescriptions' => 'Prescriptions',
+    'Post-Op Treatments' => 'Post-Op Treatments',
+    'PIM' => 'PIM',
+    'Project Management' => 'Project Management',
+    'Questions & Answers' => 'Questions & Answers',
+    'Radiology Lab' => 'Ακτινολογικό Εργαστήριο',
+    'Research Platform' => 'Πλατφόρμα Έρευνας',
+    'Robotics' => 'Ρομποτική',
+    'Symptoms Checker' => 'Symptoms Checker',
+    'Surgery' => 'Χειρουργική',
+    'Surgical Operations' => 'Surgical Operations',
+    'Tissue Sampling' => 'Tissue Sampling'
+];
---
+++ resources/lang/USA/services.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in displaying navigation
+    | (e.g. Home etc.). You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    '3D Bioprinting' => '3D Bioprinting',
+    'Accounting Analytics' => 'Accounting Analytics',
+    'BPM' => 'BPM',
+    'Business Intelligence' => 'Business Intelligence',
+    'Communication' => 'Communication',
+    'Collaboration' => 'Collaboration',
+    'CRM' => 'CRM',
+    'Diseases Diagnosis' => 'Diseases Diagnosis',
+    'Document Management' => 'Document Management',
+    'Educational Platform' => 'Educational Platform',
+    'Email' => 'Email',
+    'Emergency' => 'Emergency',
+    'ERP' => 'ERP',
+    'First Aid' => 'First Aid',
+    'Genetic Code Service' => 'Genetic Code Service',
+    'Healthcare Policies' => 'Healthcare Policies',
+    'Healthcare Servicies' =>'Healthcare Servicies',
+    'Human Resources' => 'Human Resources',
+    'Medical G.I.S.' => 'Medical G.I.S.',
+    'Medical Reports' => 'Medical Reports',
+    'Medical Lab' => 'Medical Lab',
+    'Marketing Automation' => 'Marketing Automation',
+    'Patients' => 'Patients',
+    'Patient Health Profile' => 'Patient Health Profile',
+    'Patient Monitoring' => 'Patient Monitoring',
+    'Pharmacy Lab' => 'Pharmacy Lab',
+    'Physical Treatments' => 'Physical Treatments',
+    'Prescriptions' => 'Prescriptions',
+    'Post-Op Treatments' => 'Post-Op Treatments',
+    'PIM' => 'PIM',
+    'Project Management' => 'Project Management',
+    'Questions & Answers' => 'Questions & Answers',
+    'Radiology Lab' => 'Radiology Lab',
+    'Research Platform' => 'Research Platform',
+    'Robotics' => 'Robotics',
+    'Symptoms Checker' => 'Symptoms Checker',
+    'Surgery' => 'Surgery',
+    'Surgical Operations' => 'Surgical Operations',
+    'Tissue Sampling' => 'Tissue Sampling'
+];
