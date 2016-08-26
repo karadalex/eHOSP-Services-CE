8b38c27f1bedf5bfb143d5ce21c66473dcc5871f
Alex Karadimos <karadalex@gmail.com>
2016-08-26 16:37:21 +0300

    Translate services

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,17 +25,17 @@ function service_btn($route, $title) {
                     <div class="row row-eq-height">
                         @foreach ($userTypeServices as $service => $details)
                             @if (in_array(Auth::user()->user_type, $details['userTypes']))
-                                {{ service_btn($details['route'], $service) }}
+                                {{ service_btn($details['route'], trans("services.$service")) }}
                             @endif
                         @endforeach
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0 col-md-4 col-md-offset-0 col-xl-3 col-xl-offset-0">
-                            <a href="services/emergency" class="services" style="background-color: #ec5840"> Emergency </a>
+                            <a href="services/emergency" class="services" style="background-color: #ec5840"> @lang('services.Emergency') </a>
                         </div>
                         <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0 col-md-4 col-md-offset-0 col-xl-3 col-xl-offset-0">
-                            <a href="services/first-aid" class="services" style="background-color: #ec5840"> First Aid </a>
+                            <a href="services/first-aid" class="services" style="background-color: #ec5840"> @lang('services.First Aid') </a>
                         </div>
                     </div>
                 </div>
