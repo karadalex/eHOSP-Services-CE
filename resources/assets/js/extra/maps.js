82655073c6e807281c50ae9d8ea0fe63951ff10e
Alex Karadimos <karadalex@gmail.com>
2016-08-26 16:36:45 +0300

    Add node markers on the map after all nodes json data are fetched

--- resources/assets/js/extra/maps.js
+++ resources/assets/js/extra/maps.js
@@ -3,3 +3,30 @@ var map = L.map('map', {
 }).setView([37.944, 24.115], 6);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+
+var markers = new L.FeatureGroup();
+setTimeout(function() {
+    for (var i=0; i<gis_nodes.length; i++) {
+    	var node_marker = L.marker([gis_nodes[i]["latitude"], gis_nodes[i]["longitude"]]);
+    	markers.addLayer(node_marker);
+    }
+}, 1000);
+
+
+var gis_remote_nodes_isPressed = false;
+
+function gis_remote_nodes() {
+	if (!gis_remote_nodes_isPressed) {
+		map.addLayer(markers);
+	}
+	else {
+		removeAllMarkers();
+	}
+	gis_remote_nodes_isPressed = !gis_remote_nodes_isPressed;
+	return gis_remote_nodes_isPressed;
+}
+
+function removeAllMarkers(){
+    map.removeLayer(markers);
+}
