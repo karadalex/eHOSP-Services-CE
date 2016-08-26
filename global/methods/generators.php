91d35fbd1d329ae6aab143afa44f0d210acef750
Alex Karadimos <karadalex@gmail.com>
2016-08-26 16:33:57 +0300

    Added simple button (blue) generator

--- global/methods/generators.php
+++ global/methods/generators.php
@@ -1,4 +1,12 @@
 <?php
+function btn($route, $title) {
+    $html = "
+    <div class=\"col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0 col-md-4 col-md-offset-0 col-xl-3 col-xl-offset-0\">
+        <a href=\"$route\" class=\"button full-width\"> $title </a>
+    </div>
+    ";
+    echo $html;
+}
 
 function button($button_url = null, $button_text, $classList = null, $styles = null) {
     if (isset($button_url)) {
