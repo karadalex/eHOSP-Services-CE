a0d3bb0b420e8d04e39fac828c227ebdd88a7bb8
Alex Karadimos <karadalex@gmail.com>
2016-08-27 21:08:35 +0300

    Page titles language file

---
+++ resources/lang/GRC/titles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Medical Geographic Information System' => 'Ιατρικό Σύστημα Γεωγραφικών Πληροφοριών',
+    'Welcome to eHOSP' => 'Καλωσήρθατε στο eHOSP',
+];
---
+++ resources/lang/USA/titles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Medical Geographic Information System' => 'Medical Geographic Information System',
+    'Welcome to eHOSP' => 'Welcome to eHOSP',
+];
