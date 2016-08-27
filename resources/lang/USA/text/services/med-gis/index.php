7a7c52eeca5e1d895ce717b4b3207a948e5fa600
Alex Karadimos <karadalex@gmail.com>
2016-08-27 21:11:26 +0300

    Med GIS Services index translation

---
+++ resources/lang/USA/text/services/med-gis/index.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'p1' => 'A Medical Geographic Information System or Medical GIS is a system
+            designed to capture, store, manipulate, analyze, manage, and present
+            all types of spatial or geographical data, which are related to
+            Medicine or Healthcare Services.',
+
+    'p2' => 'Click below to check the eHOSP Maps:'
+];
