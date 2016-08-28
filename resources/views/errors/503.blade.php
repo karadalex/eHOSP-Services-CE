a12fd94912603dff85b76052102056b29f24ba50
Alex Karadimos <karadalex@gmail.com>
2016-08-28 13:52:33 +0300

    Maintenance Mode

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>Be right back</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -14,10 +14,12 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
+                color: #2bb673;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato', sans-serif;
+                /*background-color: #28B09E;*/
+                background-color: #EBEBEB;
             }
 
             .container {
@@ -35,12 +37,17 @@
                 font-size: 72px;
                 margin-bottom: 40px;
             }
+
+            .comment {
+                font-size: 36px;
+            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title"> Maintenance Mode </div>
+                <div class="comment"> Please try again later ... </div>
             </div>
         </div>
     </body>
