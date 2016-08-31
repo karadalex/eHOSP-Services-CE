8bb9ba6451c01db10b06f9352f050897433a7dbe
Alex Karadimos <karadalex@gmail.com>
2016-08-31 12:59:13 +0300

    Change Language and user_type

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,8 @@ namespace eHOSP\Http\Controllers;
 use Illuminate\Http\Request;
 
 use eHOSP\Http\Requests;
+use Auth;
+use App;
 
 class SettingsController extends Controller
 {
@@ -18,6 +20,35 @@ class SettingsController extends Controller
     {
         return view('settings', [
             'title' => 'eHOSP - Settings'
+        ]);
+    }
+
+    public function changes()
+    {
+        $user = Auth::user();
+        $errors = false;
+
+        $req_user_type = request()->input('user_type');
+        if ($req_user_type != $user->user_type) {
+            $user->user_type = $req_user_type;
+            $user->save();
+        }
+        $req_language = request()->input('language');
+        if ($req_language != $user->language) {
+            $user->language = $req_language;
+            $user->save();
+            App::setLocale($req_language);
+        }
+
+        if ($errors) {
+            $message = trans('messages.settings.failure');
+        } else {
+            $message = trans('messages.settings.success');
+        }
+
+        return view('settings', [
+            'title' => 'eHOSP - Changes',
+            'message' => $message
         ]);
     }
 }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -3,8 +3,74 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <h1> @lang('nav.Settings') </h1>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1> @lang('nav.Settings') </h1>
+                </div>
+                <div class="panel-body">
+                    <form class="" action="" method="POST" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+
+                        <div class="row" style="margin-top:20px">
+                            <div class="col-md-3 col-md-offset-1">
+                                <span class="emphasize"> Profile Picture </span>
+                            </div>
+                            <div class="col-md-4">
+                                <input type="file" name="avatar" value="">
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top:10px">
+                            <div class="col-md-3 col-md-offset-1">
+                                <span class="emphasize"> Username </span>
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" name="username" placeholder="{{ Auth::user()->username }}">
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top:10px">
+                            <div class="col-md-3 col-md-offset-1">
+                                <span class="emphasize"> User Type </span>
+                            </div>
+                            <div class="col-md-4">
+                                <select class="full-width" name="user_type">
+                                    @foreach (include config('global.data').'/userTypes.php' as $userType)
+                                        <option value="{{ $userType }}" {{ Auth::user()->user_type == $userType ? "selected" : "" }}> {{ $userType }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top:10px">
+                            <div class="col-md-3 col-md-offset-1">
+                                <span class="emphasize"> Language </span>
+                            </div>
+                            <div class="col-md-4">
+                                <select class="full-width" name="language">
+                                    @foreach (include config('global.data').'/LangToLangCode.php' as $langName => $langCode)
+                                        <option value="{{ $langCode }}" {{ Auth::user()->language == $langCode ? "selected" : "" }}> {{ $langName }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary" style="width:60%;margin-top:50px;">
+                                    Save Changes
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    @if (isset($message))
+                    <div class="row" style="margin-top:20px">
+                        <div class="col-sm-10 col-sm-offset-1">
+                            <p style="text-align:center;color:#2bb673;margin-top:20px"> {{ $message }} </p>
+                        </div>
+                    </div>
+                    @endif
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
