fdf33413c2cb7a3e4659731368df85757619c61d
Alex Karadimos <karadalex@gmail.com>
2016-08-31 17:03:01 +0300

    Successfully installed Simple QrCode package (https://www.simplesoftware.io/docs/simple-qrcode, https://github.com/SimpleSoftwareIO/simple-qrcode)

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
 
         //
 
@@ -224,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
 
     ],
 
