e2b75872dc37cf08638c6fc98c5dc7f6b0f701d4
Alex Karadimos <karadalex@gmail.com>
2016-08-31 12:41:27 +0300

    Custom web app messages

---
+++ resources/lang/USA/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'settings' => [
+        'success' => 'Your changes have been saved successfully!',
+        'failure' => 'There was a problem in saving your changes. Please check your changes again or try again later'
+    ]
+];
