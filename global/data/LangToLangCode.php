8da09a16c2cf41759deaef985a43a660cbbe9b1d
Alex Karadimos <karadalex@gmail.com>
2016-08-31 12:40:12 +0300

    Data with ISO codes with languages and countries

---
+++ global/data/LangToLangCode.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Language Name to Language Code
+|--------------------------------------------------------------------------
+| Language Name converted to ISO 639-1
+|
+| Reference:
+| https://www.loc.gov/standards/iso639-2/php/code_list.php
+|
+*/
+
+return [
+    'English' => 'en',
+    'Greek' => 'gr'
+];
