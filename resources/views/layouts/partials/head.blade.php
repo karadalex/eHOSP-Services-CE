7892f69e9af2dbe3e42f65706cb0c81a40ac7be3
Alex Karadimos <karadalex@gmail.com>
2016-09-19 18:15:53 +0300

    Removed async property from script jquery

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -24,7 +24,7 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <!-- Scripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous" async=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
 <script>
     window.Laravel = <?php echo json_encode([
         'csrfToken' => csrf_token(),
