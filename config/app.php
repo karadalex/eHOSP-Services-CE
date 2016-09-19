c4aa0efa54c5fc23af6840969a3d5faf63685ff5
Alex Karadimos <karadalex@gmail.com>
2016-09-19 17:48:45 +0300

    Configure and Register Passport

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'eHOSP',
 
     /*
     |--------------------------------------------------------------------------
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Passport\PassportServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
 
         //
