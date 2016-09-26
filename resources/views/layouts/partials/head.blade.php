2191bc0e71510ffdbb1922399f8dff1b8c7bdbc5
Alex Karadimos <karadalex@gmail.com>
2016-09-26 22:47:56 +0300

    Make manifest.json recognizable by the server and the browser

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -14,6 +14,8 @@
 <meta name="keywords" content="eHOSP, Cloud, Hospital,">
 <meta name="author" content="Alex Karadimos">
 
+<link rel="manifest" href="/manifest.json">
+
 <!-- Fonts -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700"> -->
