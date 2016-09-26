3f3953c6b71adfc454712145b7f6a5b0669af04c
Alex Karadimos <karadalex@gmail.com>
2016-09-26 21:13:30 +0300

    View for the offline app

---
+++ resources/views/errors/offline.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h1> It seems you are offline! </h1>
+            <p style="text-align:center">
+                This page can only be accessed online, through an internet connection
+                <br>
+                Please connect to the internet or visit the offline pages of eHOSP.
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
