b33e859d4b38f537a4e2a48b53f79ebb724ee15a
Alex Karadimos <karadalex@gmail.com>
2016-09-26 21:14:18 +0300

    Register service worker

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -3,4 +3,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <script src="{{ asset('js/app.js') }}"></script>
 
+<script>
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('/sw.js')
+            .then(function(reg) {
+                console.log('Service Worker registered successfully!');
+            }).catch(function(err) {
+                console.log('Error?');
+            });
+    }
+</script>
+
 @yield('extraScripts')
