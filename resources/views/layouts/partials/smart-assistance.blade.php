a68ec02107678287fb47bd6659b560753f4cf65c
Alex Karadimos <karadalex@gmail.com>
2017-07-08 14:12:34 +0300

    Changed bot's name

--- resources/views/layouts/partials/smart-assistance.blade.php
+++ resources/views/layouts/partials/smart-assistance.blade.php
@@ -6,7 +6,7 @@
         <div class="invisible-container"></div>
         <div class="col-xs-7 col-sm-6 col-md-5 col-lg-4" id="smart-assistance-menu">
             <i class="fa fa-arrows-alt fa-ls bot-fullscreen" aria-hidden="true" style="color:#2bb673"></i>
-            <p style="text-align:center;padding-top:2px"> Search or talk to the eHOSP bot </p>
+            <p style="text-align:center;padding-top:2px"> Search or talk with the Hospbot </p>
             <hr>
             <bot></bot>
             <script src="{{asset('js/components.js')}}"></script>
