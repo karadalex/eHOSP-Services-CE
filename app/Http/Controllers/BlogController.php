a72e7746517ba09db27bde48c1337c111493505f
Alex Karadimos <karadalex@gmail.com>
2017-07-17 19:05:17 +0300

    Retrieve and paginate blog posts from database

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace eHOSP\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use eHOSP\Http\Requests;
 
@@ -10,8 +11,10 @@ class BlogController extends Controller
 {
     public function index()
     {
+        $blog_posts = DB::table('blog_posts')->paginate(4);
         return view('blog', [
-            'title' => 'eHOSP - Blog'
+            'title' => 'eHOSP - Blog',
+            'posts' => $blog_posts
         ]);
     }
 }
