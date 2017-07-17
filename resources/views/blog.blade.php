60700377ca6d26636f2af72d9f16443cb8a40ebe
Alex Karadimos <karadalex@gmail.com>
2017-07-17 19:06:44 +0300

    Preview all blog posts

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,6 +5,19 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <h1> @lang('nav.Blog') </h1>
+            <div class="container">
+                @foreach ($posts as $post)
+                    <a href="{{url('blog/'.kebab_case(title_case($post->title)))}}">
+                        <h3> {{$post->title}} </h3>
+                    </a>
+                    <p style="word-wrap: break-word;">
+                        {{$post->description}}
+                    </p>
+                    <br>
+                @endforeach
+                
+            </div>
+            {{ $posts->links() }}
         </div>
     </div>
 </div>
