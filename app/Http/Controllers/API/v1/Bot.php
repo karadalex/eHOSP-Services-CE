a950f4002c0ad10f6d97e05cd140bd98320aa4cc
Alex Karadimos <karadalex@gmail.com>
2017-07-21 00:05:40 +0300

    Make bot functionality publicly available

--- app/Http/Controllers/API/v1/Bot.php
+++ app/Http/Controllers/API/v1/Bot.php
@@ -12,7 +12,7 @@ class Bot extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        // $this->middleware('auth:api');
     }
 
     public function index(Request $request)
