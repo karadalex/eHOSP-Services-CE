4e11fd28346799fb303fa34710a74f3d6b456f60
Alex Karadimos <karadalex@gmail.com>
2017-07-28 23:00:02 +0300

    Added delete blog post command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\WelcomeEmail::class,
         Commands\NewBlogPost::class,
+        Commands\DeleteBlogPost::class,
     ];
 
     /**
