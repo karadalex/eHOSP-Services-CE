6572a8aca7fef449ae191e8c1035fe6640595fa2
Alex Karadimos <karadalex@gmail.com>
2017-07-29 14:00:16 +0300

    Changed how posts are displayed - Added some layout, Author, Date, Optional image and Read more link

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,17 +7,38 @@
             <h1> @lang('nav.Blog') </h1>
             <div class="container">
                 @foreach ($posts as $post)
-                    <a href="{{url('blog/'.kebab_case(title_case($post->title)))}}">
-                        <h3> {{$post->title}} </h3>
-                    </a>
-                    <p style="word-wrap: break-word;">
-                        {{$post->description}}
-                    </p>
-                    <br>
+                    <div class="col-md-8 col-md-offset-1">
+                        <a href="{{url('blog/'.kebab_case(title_case($post->title)))}}">
+                            <h3> {{$post->title}} </h3>
+                        </a>
+
+                        <?php
+                        $date = new DateTime($post->created_at);
+                        ?>
+                        <i style="font-size: 12px" class="pull-right">{{date_format($date, 'd-m-Y')}}</i>
+                        <br>
+                        <i style="font-size: 14px" class="pull-right">{{$post->author}}</i>
+                        <br><br>
+
+                        @isset($post->img)
+                            <img src="{{ $post->img }}" alt="" style="width: 100%; height: 100%">
+                        @endisset
+                        
+                        <p style="word-wrap: break-word;">
+                            {{$post->description}}
+                        </p>
+                        <h6 class="pull-right">
+                            <a href="{{url('blog/'.kebab_case(title_case($post->title)))}}"> Read more ... </a>
+                        </h6>
+                    </div>
+                    <br><br>
                 @endforeach
                 
             </div>
-            {{ $posts->links() }}
+            <div class="col-sm-5 col-sm-offset-4 col-xs-8 col-xs-offset-2">
+                {{ $posts->links() }}
+            </div>
+            <br>
         </div>
     </div>
 </div>
