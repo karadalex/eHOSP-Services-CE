b13f11fe538ada64e96c543b32f78893b3cf8ad6
Alex Karadimos <karadalex@gmail.com>
2017-07-29 18:30:34 +0300

    TODO

---
+++ app/Console/Commands/DbStoreDump.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace eHOSP\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DbStoreDump extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:dump';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a SQL dump of the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
