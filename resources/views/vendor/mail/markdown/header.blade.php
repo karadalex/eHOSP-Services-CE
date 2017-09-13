30eabf8706387b987d525fb11b93b357ad8c4c3c
Alex Karadimos <karadalex@gmail.com>
2017-09-13 21:41:24 +0300

    Add vendor view files to source control

---
+++ resources/views/vendor/mail/markdown/header.blade.php
@@ -0,0 +1,1 @@
+[![{{ $slot }}](https://marketing-image-production.s3.amazonaws.com/uploads/2af96cfef234cf356b42b48af103bf5805ed3e39aba9d172f2c2087783c8e4ea766370ab901cc2898cc51d7cceb10a4e5eb202972706a3106a74e810bd5afe5a.png)]({{ $url }})
